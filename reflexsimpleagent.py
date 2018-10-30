1576b6af6d0c5184051c09d431abfe806bf4a371
fucode <33052433+shukla284@users.noreply.github.com>
2018-10-30 22:35:30 +0530

    Contains the classes: HouseMap,Graph,VaccumReflexCleaner
    This is initial prototype file wherein it is immediate work started on this simple topic

---
+++ reflexsimpleagent.py
@@ -0,0 +1,24 @@
+class Graph(object):
+    def __init__(self,vertices,edges):
+        self.vertex=vertex
+        self.edges=edges
+        self.graph={}
+        for x in range(edges):
+            print ("Enter the adjacent vertices of the vertex "+str(x))
+            graph[vertex]=list(map(int,input().split(' ')))
+        print ("Graph accepted ")
+    def printAdjacencyList(self):
+        print ("Printing the adjacency list of the vertices ")
+        for vertex in range(vertices):
+            print(vertex+": "+graph[vertex])
+    def findShortestRouteFrom(self,vertex):
+        start=vertex
+        
+                   
+class HouseMap(object):
+    def __init__(self,
+class VaccumReflexAgent(object):
+    def __init__(self,housemap):
+        self.housemap=housemap
+    def optimizeHouseMap():
+                 
